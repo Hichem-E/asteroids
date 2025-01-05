bcad60022862f6ae42a52d0010136bbfaa1481a9
Hichem-E <hichem.elmohri@gmail.com>
2025-01-05 02:03:06 -0500

    added fps clock to maintain max 60 fps

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fpsclock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -24,6 +26,7 @@ def main():
 
 
         pygame.display.flip()
+        dt = fpsclock.tick(60)/1000
 
 
 if __name__ == "__main__":
